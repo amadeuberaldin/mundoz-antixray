8a1bf8aa86bd97973ecb3e08daa85f4543051546
Amadeu Beraldin <amadeu_beraldin@hotmail.com>
2026-07-11 17:56:36 -0300

    Implement default replacement policy

---
+++ src/main/java/com/amadeu/mundozantixray/domain/replacement/DefaultReplacementPolicy.java
@@ -0,0 +1,25 @@
+package com.amadeu.mundozantixray.domain.replacement;
+
+import com.amadeu.mundozantixray.domain.model.ReplacementDecision;
+import com.amadeu.mundozantixray.domain.model.ReplacementRepresentation;
+import com.amadeu.mundozantixray.domain.model.ReplacementResult;
+
+import java.util.Optional;
+
+public final class DefaultReplacementPolicy
+                implements ReplacementPolicy {
+
+        @Override
+        public ReplacementResult evaluate(
+                        ReplacementContext context) {
+                Objects.requireNonNull(
+                                context,
+                                "context");
+
+                return new ReplacementResult(
+                                ReplacementDecision.REPLACE,
+                                Optional.of(
+                                                new ReplacementRepresentation(
+                                                                "minecraft:stone")));
+        }
+}
